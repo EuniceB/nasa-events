3909a4a41cf5d11d88b49cc3ca73200d3dedbf26
Eunice Beijinho <nicebeijinho@gmail.com>
2020-12-04 19:38:56 +0100

    Trying to add a first test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import axios from "axios";
 import { calculateDaysSince } from "./utils";
 
 const App = () => {
+  const now = new Date();
+
   const [events, setEvents] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState({
     title: "Volcanoes",
     id: "volcanoes",
   });
-  const now = new Date();
   const [selectedTimePeriod, setSelectedTimePeriod] = useState({
     title: "This year",
     days: calculateDaysSince(new Date(now.getFullYear(), 0, 1)),
@@ -25,32 +26,34 @@ const App = () => {
     }, 2000);
   };
 
-  useEffect(() => {
-    const updateEvents = async () => {
-      let url = selectedCategory
-        ? `https://eonet.sci.gsfc.nasa.gov/api/v3/categories/${selectedCategory.id}`
-        : "https://eonet.sci.gsfc.nasa.gov/api/v3/events";
-      url += selectedTimePeriod ? "?days=" + selectedTimePeriod.days : "";
-      const { data } = await axios.get(url);
-      setEvents(data.events);
-      if (data.events.length === 0) {
-        showAlert("We found no events. Try another category or time.");
-      } else {
-        showAlert(`${data.events.length} events found.`);
-      }
-    };
+  const fetchEvents = async (categoryId, days) => {
+    let url = categoryId
+      ? `https://eonet.sci.gsfc.nasa.gov/api/v3/categories/${categoryId}`
+      : "https://eonet.sci.gsfc.nasa.gov/api/v3/events";
+    url += days ? "?days=" : "";
+    const { data } = await axios.get(url);
+    setEvents(data.events);
+  };
 
-    updateEvents();
+  const fetchCategories = async () => {
+    let url = "https://eonet.sci.gsfc.nasa.gov/api/v3/categories";
+    const { data } = await axios.get(url);
+    setCategories(data.categories);
+  };
+
+  useEffect(() => {
+    fetchEvents(
+      selectedCategory ? selectedCategory.id : null,
+      selectedTimePeriod ? selectedTimePeriod.days : null
+    );
+    if (events.length === 0) {
+      showAlert("We found no events. Try another category or time.");
+    } else {
+      showAlert(`${events.length} events found.`);
+    }
   }, [selectedCategory, selectedTimePeriod]);
 
   useEffect(() => {
-    const fetchCategories = async () => {
-      const { data } = await axios.get(
-        "https://eonet.sci.gsfc.nasa.gov/api/v3/categories"
-      );
-      setCategories(data.categories);
-    };
-
     fetchCategories();
   }, []);
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import App from "./App";
+import axios from "axios";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("axios");
+
+let container = null;
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders an alert when no events are found", async () => {
+  axios.get.mockImplementationOnce((url) => {
+    if (url.startsWith("https://eonet.sci.gsfc.nasa.gov/api/v3/categories")) {
+      console.log('returning a categories response')
+      return Promise.resolve({
+        data: {
+          categories: [{ title: "Volcanoes" }],
+        },
+      });
+    } else if (
+      url.startsWith("https://eonet.sci.gsfc.nasa.gov/api/v3/events")
+    ) {
+      console.log('returning an events response')
+      return Promise.resolve({
+        data: {
+          events: [],
+        },
+      });
+    }
+  });
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<App />, container);
+  });
+
+  expect(container.querySelector(".alert .card").textContent).toBe(
+    "We found no events. Try another category or time."
+  );
+
+  // remove the mock to ensure tests are completely isolated
+  axios.mockRestore();
 });
